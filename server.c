10990adaa3b3381b277a4c78923139bb615e9eb4
Arachan6 <35620608+Arachan6@users.noreply.github.com>
2024-11-08 21:58:07 -0300

    Update server.c
    Compila sin warnings
    Valgrind sin warnings
    Cambio prints de errores por perror
    Close socket de cliente al no poder leerlo
    Diferenciacion de casos en remove y en open

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -9,17 +10,18 @@
 
 #define PORT 5000
 #define BUFFER_SIZE 128
-#define COMMAND_SIZE 4
+#define RESPONSE_SIZE 256
 
 void close_socket(int sock){
     if (close(sock) < 0) {
-        fprintf(stderr, "No se pudo cerrar el socket\n");
+        perror("No se pudo cerrar el socket\n");
     }
 }
 
 void execute_command(const char *command, int client_socket) {
-    char response[BUFFER_SIZE];
-    char cmd[COMMAND_SIZE], key[BUFFER_SIZE], value[BUFFER_SIZE];
+    char response[RESPONSE_SIZE] = {0}; 
+    char key[BUFFER_SIZE] = {0}; 
+    char value[BUFFER_SIZE] = {0}; 
     FILE *file;
 
     if (sscanf(command, "SET %s %s", key, value) == 2) {
@@ -29,7 +31,8 @@ void execute_command(const char *command, int client_socket) {
             fclose(file);
             snprintf(response, sizeof(response), "OK\n");
         } else {
-            snprintf(response, sizeof(response), "No se pudo crear el archivo\n");
+            snprintf(response, sizeof(response), "Error del servidor\n");
+            perror("No se pudo crear el archivo\n");
         }
     } else if (sscanf(command, "GET %s", key) == 1) {
         file = fopen(key, "r");
@@ -38,13 +41,23 @@ void execute_command(const char *command, int client_socket) {
             fclose(file);
             snprintf(response, sizeof(response), "OK\n%s\n", value);
         } else {
-            snprintf(response, sizeof(response), "NOTFOUND\n");
+            if (errno == ENOENT) {
+                snprintf(response, sizeof(response), "NOTFOUND\n");
+            } else {
+                snprintf(response, sizeof(response), "Error del servidor\n");
+                perror("No se pudo abrir el archivo\n");
+            }
         }
     } else if (sscanf(command, "DEL %s", key) == 1) {
         if (remove(key) == 0) {
             snprintf(response, sizeof(response), "OK\n");
         } else {
-            snprintf(response, sizeof(response), "OK\n");
+            if (errno == ENOENT) {
+                snprintf(response, sizeof(response), "OK\n");
+            } else {
+                snprintf(response, sizeof(response), "Error del servidor\n");
+                perror("No se pudo remover el archivo\n");
+            }
         }
     } else {
         fprintf(stdout, "Comando no reconocido\n");
@@ -57,11 +70,12 @@ void handle_client(int client_socket) {
     char buffer[BUFFER_SIZE];
     int n = read(client_socket, buffer, BUFFER_SIZE - 1);
     if (n < 0) {
-        fprintf(stderr, "No se pudo leer los datos enviados por el cliente\n");
+        perror("No se pudo leer los datos enviados por el cliente\n");
+        close_socket(client_socket); 
         return;
     }
     buffer[n] = '\0';
-    printf("Mensaje recibido: %s\n", buffer);
+    fprintf(stdout, "Mensaje recibido: %s\n", buffer);
 
     execute_command(buffer, client_socket);
     close_socket(client_socket); 
@@ -72,7 +86,7 @@ int main(void) {
     // Se crea el socket del servidor
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
-        fprintf(stderr, "No se pudo crear el socket del servidor\n");
+        perror("No se pudo crear el socket del servidor\n");
         exit(EXIT_FAILURE);
     }
 
@@ -85,20 +99,20 @@ int main(void) {
 
     // Se asocia el socket a la dirección y puerto especificados en serveraddr
     if (bind(server_socket, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1) {
-        fprintf(stderr, "No se pudo abrir el socket del servidor\n");
+        perror("No se pudo abrir el socket del servidor\n");
         close_socket(server_socket); 
         exit(EXIT_FAILURE);
     }
 
     // Se configura el socket para escuchar conexiones entrantes
     if (listen(server_socket, 1) < 0) {
-        fprintf(stderr, "No se pudo configurar el socket en listen\n");
+        perror("No se pudo configurar el socket en listen\n");
         close_socket(server_socket); 
         exit(EXIT_FAILURE);
     }
 
-    printf("Servidor TCP activo\n");
-    printf("Esperando conexiones en el puerto %d...\n", PORT);
+    fprintf(stdout, "Servidor TCP activo\n");
+    fprintf(stdout, "Esperando conexiones en el puerto %d...\n", PORT);
 
     while (1) {
         // Se espera la conexión de algun cliente
@@ -106,14 +120,14 @@ int main(void) {
         socklen_t addr_len = sizeof(struct sockaddr_in);
         int client_socket = accept(server_socket, (struct sockaddr *)&clientaddr, &addr_len);
         if (client_socket < 0) {
-            fprintf(stderr, "No se pudo aceptar conexión entrante\n");
+            perror("No se pudo aceptar conexión entrante\n");
             continue;
         }
 
         // Se imprime dirección de cliente
         char ipClient[32];
         inet_ntop(AF_INET, &(clientaddr.sin_addr), ipClient, sizeof(ipClient));
-        fprintf(stderr, "Cliente conectado desde:  %s\n", ipClient);
+        fprintf(stdout, "Cliente conectado desde:  %s\n", ipClient);
 
         handle_client(client_socket);
     }
